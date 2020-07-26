4d19b6cbaa16b3558bbcc015472655cd550e880e
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-07-26 13:58:41 +0300

    perpar to deploy on heroku

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Decalaring varaibles
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+
+// initialize the server
+const server = express();
+server.use(cors());
+
+
+
+// Declare a port
+const PORT = process.env.PORT || 3000;
+
+// Test the server
+server.listen(PORT, () => {
+  console.log('I am listening to port: ', PORT);
+});
+
+
+// localhost:3000/test
+server.get('/test', (request, response) => {
+  response.send('You are awesome!');
+});
