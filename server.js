b3877d60f8b5e23a2b957871780c6dddc641fdab
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-07-26 15:02:26 +0300

    update

--- server.js
+++ server.js
@@ -26,9 +26,30 @@ server.get('/location', (request, response) => {
   response.status(status).send(getLocation);
 });
 
+// localhost:3010/weather
+server.get('/weather', (request, response) => {
+  let city = request.query.city;
+  let status = 200;
+  let weatherData = require('./data/weather.json');
+  let getLocation = new Location(city, weatherData);
+  response.status(status).send(getLocation);
+});
+
+
+
+// constructor function formate the location responed data
 function Location(city, data) {
   this.search_query = city;
   this.formatted_query = data[0].display_name;
   this.latitude = data[0].lat;
   this.longitude = data[0].lon;
 }
+
+// constructor function formate the weather responed data
+let weathers = [];
+function Weather(city, data) {
+  this.forecast = data.weather.description;
+  this.time = data.valid_date;
+  weathers.push(this);
+}
+
